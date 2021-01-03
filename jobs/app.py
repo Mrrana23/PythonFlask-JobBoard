eafe0c9b724c7dd58e6dbc57af8fa9c649029bb5
Mrrana23 <Mrrana23@gmail.com>
2021-01-03 14:35:55 -0800

    Davids update

--- jobs/app.py
+++ jobs/app.py
@@ -9,10 +9,10 @@ def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
-        connection.row_factory = sqlite.Row
-        return connection
+    connection.row_factory = sqlite.Row
+    return connection
 
-def execute_sql(sql, values=(), commit=False,single=False):
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit == True:
